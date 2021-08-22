a4c3bf4054f37236f172802f112b09fd694eac9a
iSailDev <89343984+iSailDev@users.noreply.github.com>
2021-08-22 22:13:07 +0200

    Flask draft

---
+++ flask/flaskapp.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+# TODO: Fetch GIFs from database
+gifs = ['https://c.tenor.com/54UdL_zUuG0AAAAC/bo-bech-med-kniven-for-struben.gif', 'https://c.tenor.com/0yhDLNgdffIAAAAC/med-kniven-for-struben-bo-bech.gif', 'https://c.tenor.com/OQyn2OuUrx4AAAAd/bo-bech-med-kniven-for-struben.gif', 
+        'https://c.tenor.com/xrT1B6ZMI44AAAAd/bo-bech-med-kniven-for-struben.gif', 'https://c.tenor.com/xleCMjEzHOUAAAAC/med-kniven-for-struben-bo-bech.gif', 'https://c.tenor.com/DMumjMaVEO0AAAAd/bo-bech-med-kniven-for-struben.gif']
+# TODO: Fetch moods from database
+moods = ['angry', 'annoyed', 'cheerful', 'disappointed', 'disgusted', 'funny', 'happy', 'negative', 'romantic', 'sad', 'serious', 'surprised']
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', gifs=gifs, moods=moods)
+
+if __name__ == '__main__':
+    app.run(debug=True)
