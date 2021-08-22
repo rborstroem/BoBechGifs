32a85345b5aa3fd105b77be3f19a2aeefc5d6e78
rb <rasmus@borstroem.dk>
2021-08-23 00:49:02 +0200

    trying to access everything

--- flask/flaskapp.py
+++ flask/flaskapp.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 import sys
+import math
+import requests
 
 # Load env variables
 load_dotenv()
@@ -53,12 +55,30 @@ results = db.session.query(gifmood).all()
 
 ids, moods = zip(*results)
 
+moods = list(set(moods))
+ids = list(set(ids))
+gifs = []
+
+max_ids = 50
+number_of_iterations = math.floor(len(ids) / max_ids)
+
+for i in range(number_of_iterations):
+    id_strings = ",".join(ids[i*max_ids:(i+1)*max_ids])
+    response = requests.get('https://g.tenor.com/v1/gifs?media_filter=minimal&key='+key+'&ids='+str(id_strings)).json()
+
+    for result in response['results']:
+        gifs.append(result['url'])
+
+# REMAINDER
+remaining = len(ids) - max_ids * number_of_iterations
+id_strings = ",".join(ids[len(ids) - remaining:])
+response = requests.get('https://g.tenor.com/v1/gifs?media_filter=minimal&key='+key+'&ids='+str(id_strings)).json()
+for result in response['results']:
+    gifs.append(result['url'])
 
 # TODO: Fetch GIFs from database
-gifs = ['https://c.tenor.com/54UdL_zUuG0AAAAC/bo-bech-med-kniven-for-struben.gif', 'https://c.tenor.com/0yhDLNgdffIAAAAC/med-kniven-for-struben-bo-bech.gif', 'https://c.tenor.com/OQyn2OuUrx4AAAAd/bo-bech-med-kniven-for-struben.gif', 
-        'https://c.tenor.com/xrT1B6ZMI44AAAAd/bo-bech-med-kniven-for-struben.gif', 'https://c.tenor.com/xleCMjEzHOUAAAAC/med-kniven-for-struben-bo-bech.gif', 'https://c.tenor.com/DMumjMaVEO0AAAAd/bo-bech-med-kniven-for-struben.gif']
-# TODO: Fetch moods from database
-# moods = ['angry', 'annoyed', 'cheerful', 'disappointed', 'disgusted', 'funny', 'happy', 'negative', 'romantic', 'sad', 'serious', 'surprised']
+#gifs = ['https://c.tenor.com/54UdL_zUuG0AAAAC/bo-bech-med-kniven-for-struben.gif', 'https://c.tenor.com/0yhDLNgdffIAAAAC/med-kniven-for-struben-bo-bech.gif', 'https://c.tenor.com/OQyn2OuUrx4AAAAd/bo-bech-med-kniven-for-struben.gif', 
+#        'https://c.tenor.com/xrT1B6ZMI44AAAAd/bo-bech-med-kniven-for-struben.gif', 'https://c.tenor.com/xleCMjEzHOUAAAAC/med-kniven-for-struben-bo-bech.gif', 'https://c.tenor.com/DMumjMaVEO0AAAAd/bo-bech-med-kniven-for-struben.gif']
 
 @app.route('/')
 @app.route('/home')
