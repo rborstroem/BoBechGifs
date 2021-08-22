c56a6f04128581257b423066f74d5f34608dc09c
rb <rasmus@borstroem.dk>
2021-08-23 01:19:23 +0200

    added alt gif link for clipboard

--- flask/flaskapp.py
+++ flask/flaskapp.py
@@ -55,8 +55,9 @@ results = db.session.query(gifmood).all()
 
 ids, moods = zip(*results)
 
-moods = list(set(moods))
-ids = list(set(ids))
+moods = list(set(moods)).sort()
+ids = list(set(ids)).sort()
+preview_gifs = []
 gifs = []
 
 max_ids = 50
@@ -67,23 +68,21 @@ for i in range(number_of_iterations):
     response = requests.get('https://g.tenor.com/v1/gifs?media_filter=minimal&key='+key+'&ids='+str(id_strings)).json()
 
     for result in response['results']:
-        gifs.append(result['media'][0]['tinygif']['url'])
+        preview_gifs.append(result['media'][0]['tinygif']['url'])
+        gifs.append(result['url'])
 
 # REMAINDER
 remaining = len(ids) - max_ids * number_of_iterations
 id_strings = ",".join([str(i) for i in ids[len(ids) - remaining:]])
 response = requests.get('https://g.tenor.com/v1/gifs?media_filter=minimal&key='+key+'&ids='+str(id_strings)).json()
 for result in response['results']:
-    gifs.append(result['media'][0]['tinygif']['url'])
-
-# TODO: Fetch GIFs from database
-#gifs = ['https://c.tenor.com/54UdL_zUuG0AAAAC/bo-bech-med-kniven-for-struben.gif', 'https://c.tenor.com/0yhDLNgdffIAAAAC/med-kniven-for-struben-bo-bech.gif', 'https://c.tenor.com/OQyn2OuUrx4AAAAd/bo-bech-med-kniven-for-struben.gif', 
-#        'https://c.tenor.com/xrT1B6ZMI44AAAAd/bo-bech-med-kniven-for-struben.gif', 'https://c.tenor.com/xleCMjEzHOUAAAAC/med-kniven-for-struben-bo-bech.gif', 'https://c.tenor.com/DMumjMaVEO0AAAAd/bo-bech-med-kniven-for-struben.gif']
+    preview_gifs.append(result['media'][0]['tinygif']['url'])
+    gifs.append(result['url'])
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', gifs=gifs, moods=moods)
+    return render_template('home.html', preview_gifs=preview_gifs, moods=moods, gifs=gifs)
 
 if __name__ == '__main__':
     app.run(debug=True)
