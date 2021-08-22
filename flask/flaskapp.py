84eca04ebe842952fb7f721dac6ae7a2635f4e1b
rb <rasmus@borstroem.dk>
2021-08-22 23:51:59 +0200

    testing db

--- flask/flaskapp.py
+++ flask/flaskapp.py
@@ -6,7 +6,36 @@ import sys
 # Load env variables
 load_dotenv()
 
-token = os.getenv('TEST')
+# Get env variables
+key = os.getenv('TENOR_KEY')
+db_user = os.getenv('DB_USER')
+db_pass = os.getenv('DB_PASS')
+db_host = os.getenv('DB_HOST')
+db_db = os.getenv('DB_DB')
+
+# Try to connect to database
+# If errors occurs, set use_db to false
+try: 
+    conn = mariadb.connect(
+        user=db_user,
+        password=db_pass,
+        host=db_host,
+        port=3306,
+        database=db_db
+    )
+
+    cur = conn.cursor()
+    
+    cur.execute("SELECT ID, MoodType FROM GifMood")
+
+    ids, moods = zip(*cur)
+
+
+
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB platform: {e}")
+
+
 
 app = Flask(__name__)
 
@@ -14,12 +43,12 @@ app = Flask(__name__)
 gifs = ['https://c.tenor.com/54UdL_zUuG0AAAAC/bo-bech-med-kniven-for-struben.gif', 'https://c.tenor.com/0yhDLNgdffIAAAAC/med-kniven-for-struben-bo-bech.gif', 'https://c.tenor.com/OQyn2OuUrx4AAAAd/bo-bech-med-kniven-for-struben.gif', 
         'https://c.tenor.com/xrT1B6ZMI44AAAAd/bo-bech-med-kniven-for-struben.gif', 'https://c.tenor.com/xleCMjEzHOUAAAAC/med-kniven-for-struben-bo-bech.gif', 'https://c.tenor.com/DMumjMaVEO0AAAAd/bo-bech-med-kniven-for-struben.gif']
 # TODO: Fetch moods from database
-moods = ['angry', 'annoyed', 'cheerful', 'disappointed', 'disgusted', 'funny', 'happy', 'negative', 'romantic', 'sad', 'serious', 'surprised']
+# moods = ['angry', 'annoyed', 'cheerful', 'disappointed', 'disgusted', 'funny', 'happy', 'negative', 'romantic', 'sad', 'serious', 'surprised']
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', gifs=gifs, moods=moods, token=token)
+    return render_template('home.html', gifs=gifs, moods=moods)
 
 if __name__ == '__main__':
     app.run(debug=True)
