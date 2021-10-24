bbf88a2a6adc6d73d29afcbdcaf87cd3f4462954
iSailDev <89343984+iSailDev@users.noreply.github.com>
2021-10-24 22:55:11 +0200

    Removed redundant env vars

--- flask/flaskapp.py
+++ flask/flaskapp.py
@@ -11,19 +11,15 @@ import requests
 load_dotenv()
 
 # Get env variables
-key = os.getenv('TENOR_KEY')
-db_user = os.getenv('DB_USER')
-db_pass = os.getenv('DB_PASS')
-db_host = os.getenv('DB_HOST')
-db_db = os.getenv('DB_DB')
-maria_db = os.getenv('JAWSDB_MARIA_URL')
-
+tenor_key = os.getenv('TENOR_KEY')
+jawsdb_maria_url = os.getenv('JAWSDB_MARIA_URL')
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = jawsdb_maria_url
+
 Talisman(app, content_security_policy=None)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = maria_db
 db = SQLAlchemy(app)
 
 gifmood = db.Table('GifMood', db.metadata, autoload=True, autoload_with=db.engine)
@@ -63,7 +59,7 @@ for i in range(iterations):
     # 2. ["1","2","3",...] (str list comprehension)
     # 3. "1,2,3,..." (join)
     id_strings = ",".join([str(id_no) for id_no in ids[i*tenor_limit:(i+1)*tenor_limit]])
-    request_string = 'https://g.tenor.com/v1/gifs?media_filter=minimal&key=' + key + '&ids=' + str(id_strings)
+    request_string = 'https://g.tenor.com/v1/gifs?media_filter=minimal&key=' + tenor_key + '&ids=' + str(id_strings)
     response = requests.get(request_string).json()
 
     for result in response['results']:
